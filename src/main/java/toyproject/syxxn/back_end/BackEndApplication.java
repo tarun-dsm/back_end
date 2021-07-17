b90098a0d51cf27388750d4bad1c8ae84a52574c
이승윤 <yyuunn17@naver.com>
2021-07-17 19:31:29 +0900

    Add init

---
+++ src/main/java/toyproject/syxxn/back_end/BackEndApplication.java
@@ -0,0 +1,13 @@
+package toyproject.syxxn.back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyproject/syxxn/back_end/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.syxxn.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
