eee13e7c134e244c0931146648e20a7c2acc1e91
이승윤 <yyuunn17@naver.com>
2021-07-21 20:43:17 +0900

    Update

--- src/test/java/toyproject/syxxn/back_end/AccountControllerTest.java
+++ src/test/java/toyproject/syxxn/back_end/AccountControllerTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import toyproject.syxxn.back_end.config.EmbeddedRedisConfig;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {BackEndApplication.class, EmbeddedRedisConfig.class})
+@SpringBootTest(classes = BackEndApplication.class)
 @ActiveProfiles("test")
 public class AccountControllerTest {
 
