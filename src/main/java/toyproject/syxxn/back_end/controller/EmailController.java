f701ff806778e1dd328c7a5bf4e91f3daecd679a
이승윤 <yyuunn17@naver.com>
2021-07-22 01:02:14 +0900

    Update

--- src/main/java/toyproject/syxxn/back_end/controller/EmailController.java
+++ src/main/java/toyproject/syxxn/back_end/controller/EmailController.java
@@ -1,16 +1,13 @@
 package toyproject.syxxn.back_end.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import toyproject.syxxn.back_end.dto.request.EmailRequest;
-import toyproject.syxxn.back_end.dto.request.SignInRequest;
 import toyproject.syxxn.back_end.dto.request.VerifyRequest;
-import toyproject.syxxn.back_end.dto.response.AccessTokenResponse;
-import toyproject.syxxn.back_end.dto.response.TokenResponse;
 import toyproject.syxxn.back_end.service.email.EmailService;
 
 import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/email")
@@ -20,13 +17,12 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping
-    public void sendVerifyNumberEmail(EmailRequest request) {
+    public void sendVerifyNumberEmail(@RequestBody @Valid EmailRequest request) {
         emailService.sendVerifyNumberEmail(request.getEmail());
     }
 
     @PatchMapping
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void verify(VerifyRequest request) {
+    public void verify(@RequestBody @Valid VerifyRequest request) {
         emailService.verify(request);
     }
 
--- src/main/java/toyproject/syxxn/back_end/service/email/EmailService.java
+++ src/main/java/toyproject/syxxn/back_end/service/email/EmailService.java
@@ -1,6 +1,5 @@
 package toyproject.syxxn.back_end.service.email;
 
-import toyproject.syxxn.back_end.dto.request.EmailRequest;
 import toyproject.syxxn.back_end.dto.request.VerifyRequest;
 
 public interface EmailService {
