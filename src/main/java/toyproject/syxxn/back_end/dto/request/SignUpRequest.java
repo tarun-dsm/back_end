9264b86995682ef96bec1ebb81c18564cc514eac
이승윤 <yyuunn17@naver.com>
2021-07-21 12:08:14 +0900

    Update dto

--- src/main/java/toyproject/syxxn/back_end/dto/request/SignUpRequest.java
+++ src/main/java/toyproject/syxxn/back_end/dto/request/SignUpRequest.java
@@ -1,38 +1,42 @@
 package toyproject.syxxn.back_end.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
-@NoArgsConstructor
+@Builder
 @AllArgsConstructor
 public class SignUpRequest {
 
     @Email
-    private String email;
+    @NotNull
+    private final String email;
 
     @NotNull
-    private String password;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#*_])[a-zA-Z0-9~!@#*_]{8,15}$")
+    private final String password;
 
     @NotNull
-    private String nickname;
+    private final String nickname;
 
     @NotNull
-    private int age;
+    private final int age;
 
     @NotNull
-    private String sex;
+    private final String sex;
 
     @NotNull
-    private boolean isExperienceRasingPet;
+    private final boolean isExperienceRasingPet;
 
-    private String experience;
+    private final String experience;
 
     @NotNull
-    private String address;
+    private final String address;
 
 }
