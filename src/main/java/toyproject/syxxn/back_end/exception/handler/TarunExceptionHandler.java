f603a82651685babb0aa5038e8a54907cfd971f1
이승윤 <yyuunn17@naver.com>
2021-07-23 12:29:35 +0900

    Update
    ControllerAdvice 밑에는 여러 ExceptionHandler가 들어갈 수 있다.

--- src/main/java/toyproject/syxxn/back_end/exception/handler/TarunExceptionHandler.java
+++ src/main/java/toyproject/syxxn/back_end/exception/handler/TarunExceptionHandler.java
@@ -5,12 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ValidationException;
+
 @ControllerAdvice
 public class TarunExceptionHandler {
 
     @ExceptionHandler(TarunException.class)
     protected ResponseEntity<ErrorResponse> handleTarunException(final TarunException e) {
         return new ResponseEntity<>(new ErrorResponse(e.getStatus(), e.getMessage()), HttpStatus.valueOf(e.getStatus()));
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    protected ResponseEntity<ErrorResponse> handleValidException() {
+        return new ResponseEntity<>(new ErrorResponse(400, "Invalid value"), HttpStatus.valueOf(400));
     }
 
 }
