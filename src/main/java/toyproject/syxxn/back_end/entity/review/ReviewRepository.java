900d4389465eb681f00e47b76adbea0be15960f9
이승윤 <yyuunn17@naver.com>
2021-07-24 15:44:59 +0900

    Delete review
    공사 필요

--- src/main/java/toyproject/syxxn/back_end/entity/review/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package toyproject.syxxn.back_end.entity.review;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends CrudRepository<Review, Long> {
-}
