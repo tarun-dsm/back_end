3b180adf2d2bfe7c74678e1e53eddd7621373234
이승윤 <yyuunn17@naver.com>
2021-07-24 13:30:29 +0900

    Add annotation

--- src/main/java/toyproject/syxxn/back_end/BackEndApplication.java
+++ src/main/java/toyproject/syxxn/back_end/BackEndApplication.java
@@ -2,7 +2,9 @@ package toyproject.syxxn.back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackEndApplication {
 
