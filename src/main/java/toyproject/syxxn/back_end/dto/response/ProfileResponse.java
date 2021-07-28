be84325788c95c7c2a9bf5edc3690ff54a747dfe
이승윤 <yyuunn17@naver.com>
2021-07-28 17:29:25 +0900

    Add dto

---
+++ src/main/java/toyproject/syxxn/back_end/dto/response/ProfileResponse.java
@@ -0,0 +1,34 @@
+package toyproject.syxxn.back_end.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileResponse {
+
+    private Boolean isMyself;
+
+    private String nickname;
+
+    private Integer age;
+
+    private String sex;
+
+    private boolean isExperienceRasingPet;
+
+    private String experience;
+
+    private boolean isLocationConfirm;
+
+    private BigDecimal avgGrade;
+
+    private String rating;
+
+}
