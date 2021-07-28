94708781f47b1919d00e48d586ccf04c9420aa32
이승윤 <yyuunn17@naver.com>
2021-07-29 00:22:03 +0900

    Update

--- src/main/java/toyproject/syxxn/back_end/entity/post/PostRepository.java
+++ src/main/java/toyproject/syxxn/back_end/entity/post/PostRepository.java
@@ -2,7 +2,11 @@ package toyproject.syxxn.back_end.entity.post;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import toyproject.syxxn.back_end.entity.account.Account;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends CrudRepository<Post, Integer> {
+    List<Post> findAllByAccountOrderByCreatedAtDesc(Account account);
 }
