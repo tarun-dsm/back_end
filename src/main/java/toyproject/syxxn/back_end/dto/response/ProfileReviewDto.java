ab3ced3843779fbafd375576be64a9b8f78974fb
이승윤 <yyuunn17@naver.com>
2021-07-28 23:51:36 +0900

    Add Dto

---
+++ src/main/java/toyproject/syxxn/back_end/dto/response/ProfileReviewDto.java
@@ -0,0 +1,27 @@
+package toyproject.syxxn.back_end.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileReviewDto {
+
+    private Integer id;
+
+    private String nickName;
+
+    private BigDecimal grade;
+
+    private String comment;
+
+    private LocalDateTime createdAt;
+
+}
