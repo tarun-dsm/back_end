e393041d806ad1b47b333f64ead830a66fae27fe
이승윤 <yyuunn17@naver.com>
2021-07-28 11:20:02 +0900

    Update 인증객체가 없는 경우 401 띄우기

--- src/main/java/toyproject/syxxn/back_end/security/auth/AuthenticationFacade.java
+++ src/main/java/toyproject/syxxn/back_end/security/auth/AuthenticationFacade.java
@@ -1,13 +1,19 @@
 package toyproject.syxxn.back_end.security.auth;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import toyproject.syxxn.back_end.exception.UserNotUnauthenticatedException;
 
 @Component
 public class AuthenticationFacade {
 
     public String getUserEmail() {
-        return SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new UserNotUnauthenticatedException();
+        }
+        return authentication.getName();
     }
 
 }
