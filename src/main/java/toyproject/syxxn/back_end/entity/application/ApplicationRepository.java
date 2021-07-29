d1958512858acba27d03e3411a58240e32731221
이승윤 <yyuunn17@naver.com>
2021-07-29 20:00:41 +0900

    Update

--- src/main/java/toyproject/syxxn/back_end/entity/application/ApplicationRepository.java
+++ src/main/java/toyproject/syxxn/back_end/entity/application/ApplicationRepository.java
@@ -1,6 +1,6 @@
 package toyproject.syxxn.back_end.entity.application;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import toyproject.syxxn.back_end.entity.account.Account;
 import toyproject.syxxn.back_end.entity.post.Post;
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ApplicationRepository extends JpaRepository<Application, Integer> {
+public interface ApplicationRepository extends CrudRepository<Application, Integer> {
     Optional<Application> findByAccountAndPost(Account account, Post post);
     List<Application> findAllByAccount(Account account);
     List<Application> findAllByPost(Post post);
