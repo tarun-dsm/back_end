1060a6c4206e5831bcfc2ad25c48a99ddac67cdb
이승윤 <yyuunn17@naver.com>
2021-07-30 16:45:49 +0900

    Add dto

---
+++ src/main/java/toyproject/syxxn/back_end/dto/request/CoordinatesRequest.java
@@ -0,0 +1,23 @@
+package toyproject.syxxn.back_end.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoordinatesRequest {
+
+    @NotNull
+    private BigDecimal longitude;
+
+    @NotNull
+    private BigDecimal latitude;
+
+}
