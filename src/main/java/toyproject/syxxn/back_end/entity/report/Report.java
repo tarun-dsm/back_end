07ef04e28969f3b92b4032f2ca90b7924b5b85a9
이승윤 <yyuunn17@naver.com>
2021-08-30 17:14:50 +0900

    Add Report dao

---
+++ src/main/java/toyproject/syxxn/back_end/entity/report/Report.java
@@ -0,0 +1,35 @@
+package toyproject.syxxn.back_end.entity.report;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import toyproject.syxxn.back_end.entity.BaseCreatedAtEntity;
+import toyproject.syxxn.back_end.entity.account.Account;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Report extends BaseCreatedAtEntity {
+
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "target_id", nullable = false)
+    private Account target;
+
+    @ManyToOne
+    @JoinColumn(name = "reporter_id", nullable = false)
+    private Account reporter;
+
+    @Builder
+    public Report(String comment, Account target, Account reporter) {
+        this.comment = comment;
+        this.target = target;
+        this.reporter = reporter;
+    }
+
+}
---
+++ src/main/java/toyproject/syxxn/back_end/entity/report/ReportRepository.java
@@ -0,0 +1,6 @@
+package toyproject.syxxn.back_end.entity.report;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReportRepository extends CrudRepository<Report, Integer> {
+}
