f2ace5426cf1480cc52ef45f19b47840384351c9
이승윤 <yyuunn17@naver.com>
2021-10-06 16:20:20 +0900

    [ADD] comment entity 추가

---
+++ src/main/java/toyproject/syxxn/back_end/entity/comment/Comment.java
@@ -0,0 +1,51 @@
+package toyproject.syxxn.back_end.entity.comment;
+
+import com.querydsl.core.annotations.QueryEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import toyproject.syxxn.back_end.entity.BaseCreatedAtEntity;
+import toyproject.syxxn.back_end.entity.account.Account;
+import toyproject.syxxn.back_end.entity.post.Post;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@QueryEntity
+public class Comment extends BaseCreatedAtEntity {
+
+    @Column(length = 100, nullable = false)
+    private String comment;
+
+    private Boolean isUpdated;
+
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account writer;
+
+    @Builder
+    public Comment(String comment, Post post, Account account) {
+        this.comment = comment;
+        this.post = post;
+        this.writer = account;
+        this.isUpdated = false;
+    }
+
+    public Comment updateComment(String comment) {
+        this.comment = comment;
+        this.isUpdated = true;
+        this.updatedAt = LocalDateTime.now();
+        return this;
+    }
+
+}
---
+++ src/main/java/toyproject/syxxn/back_end/entity/comment/CommentRepository.java
@@ -0,0 +1,6 @@
+package toyproject.syxxn.back_end.entity.comment;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentRepository extends CrudRepository<Comment, Integer> {
+}
