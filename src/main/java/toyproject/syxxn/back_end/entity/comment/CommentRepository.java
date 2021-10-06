30162932f383a925b40c53740b335b695b8a0adb
이승윤 <yyuunn17@naver.com>
2021-10-06 19:23:42 +0900

    [ADD] JPA method

--- src/main/java/toyproject/syxxn/back_end/entity/comment/CommentRepository.java
+++ src/main/java/toyproject/syxxn/back_end/entity/comment/CommentRepository.java
@@ -1,6 +1,10 @@
 package toyproject.syxxn.back_end.entity.comment;
 
 import org.springframework.data.repository.CrudRepository;
+import toyproject.syxxn.back_end.entity.post.Post;
+
+import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
+    List<Comment> findAllByPostOrderByCreatedAtDesc(Post post);
 }
