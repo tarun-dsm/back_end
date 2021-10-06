d3e77cc5a67283bf6b55b785765d170543f77542
이승윤 <yyuunn17@naver.com>
2021-10-06 19:28:54 +0900

    [ADD] @Max

--- src/main/java/toyproject/syxxn/back_end/dto/request/CommentRequest.java
+++ src/main/java/toyproject/syxxn/back_end/dto/request/CommentRequest.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 
 @Getter
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class CommentRequest {
 
+    @Max(100)
     @NotBlank
     private String comment;
 
