aaa863d569a68653fc10f4b608f290efcb220e8d
이승윤 <yyuunn17@naver.com>
2021-11-01 23:07:24 +0900

    [UPDATE] 혹시나 해서

--- src/main/java/toyproject/syxxn/back_end/entity/BaseCreatedAtEntity.java
+++ src/main/java/toyproject/syxxn/back_end/entity/BaseCreatedAtEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseCreatedAtEntity extends BaseIdEntity{
+public class BaseCreatedAtEntity extends BaseIdEntity{
 
     @CreatedDate
     @DateTimeFormat(pattern = "yyyy-MM-dd`T`hh:mm:ss")
