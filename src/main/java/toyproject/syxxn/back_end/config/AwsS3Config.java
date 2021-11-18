17a6d3d59fd713e4daaee12dceb4265f5edf2089
이승윤 <yyuunn17@naver.com>
2021-11-18 22:32:35 +0900

    [FIX] S3Client error

--- src/main/java/toyproject/syxxn/back_end/config/AwsS3Config.java
+++ src/main/java/toyproject/syxxn/back_end/config/AwsS3Config.java
@@ -2,7 +2,9 @@ package toyproject.syxxn.back_end.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +21,14 @@ public class AwsS3Config {
 
     @Value("${cloud.aws.region.static}")
     private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
 
     @Bean
     public AmazonS3 amazonS3() {
