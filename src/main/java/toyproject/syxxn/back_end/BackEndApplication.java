65a710e467d7d08a8cd66de47c120dadc7c64cdc
이승윤 <yyuunn17@naver.com>
2021-11-27 15:58:35 +0900

    [UPDATE] @PostConstruct로 TimeZone 설정해볼게요

--- src/main/java/toyproject/syxxn/back_end/BackEndApplication.java
+++ src/main/java/toyproject/syxxn/back_end/BackEndApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
 	}
 
 }
