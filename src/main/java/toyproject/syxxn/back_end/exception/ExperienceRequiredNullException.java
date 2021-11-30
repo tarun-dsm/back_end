d168c26bf1832f3b0d2a4b60c5e9d9ec32591bd0
이승윤 <yyuunn17@naver.com>
2021-12-01 08:47:31 +0900

    [UPDATE] status 코드 다르게 주기 418

--- src/main/java/toyproject/syxxn/back_end/exception/ExperienceRequiredNullException.java
+++ src/main/java/toyproject/syxxn/back_end/exception/ExperienceRequiredNullException.java
@@ -4,6 +4,6 @@ import toyproject.syxxn.back_end.exception.handler.TarunException;
 
 public class ExperienceRequiredNullException extends TarunException {
     public ExperienceRequiredNullException() {
-        super(400, "When the value 'isExperienceRasingPet' is false, the value 'experience' must be null.");
+        super(418, "When the value 'isExperienceRasingPet' is false, the value 'experience' must be null.");
     }
 }
