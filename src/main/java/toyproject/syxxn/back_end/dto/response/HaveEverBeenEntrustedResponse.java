595fbb587f84635f55befd947a1fc58522bdc9e7
이승윤 <yyuunn17@naver.com>
2021-11-30 16:16:55 +0900

    [UPDATE] key값 변경

--- src/main/java/toyproject/syxxn/back_end/dto/response/HaveEverBeenEntrustedResponse.java
+++ src/main/java/toyproject/syxxn/back_end/dto/response/HaveEverBeenEntrustedResponse.java
@@ -1,5 +1,6 @@
 package toyproject.syxxn.back_end.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class HaveEverBeenEntrustedResponse {
-    private boolean isTrue;
+    @JsonProperty("is_existence")
+    private boolean isExistence;
 }
