6e6813b1450504413ec00c451aa1e56229387061
이승윤 <yyuunn17@naver.com>
2021-12-08 19:13:04 +0900

    ⚰️ :: FileNotFoundException 클래스 삭제

--- src/main/java/toyproject/syxxn/back_end/exception/FileNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package toyproject.syxxn.back_end.exception;
-
-import toyproject.syxxn.back_end.exception.handler.TarunException;
-
-public class FileNotFoundException extends TarunException {
-    public FileNotFoundException() {
-        super(400, "File is not found.");
-    }
-}
