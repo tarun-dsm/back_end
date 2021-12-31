c093d3ec7866cd58bb8eaee9e6a2192065e00300
이승윤 <yyuunn17@naver.com>
2021-12-31 09:37:45 +0900

    🧪 :: Post 엔티티 테스트 추가

---
+++ src/test/java/toyproject/syxxn/back_end/entity/post/PostTest.java
@@ -0,0 +1,53 @@
+package toyproject.syxxn.back_end.entity.post;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import toyproject.syxxn.back_end.dto.request.PostRequest;
+import toyproject.syxxn.back_end.entity.GetAccount;
+import toyproject.syxxn.back_end.entity.GetPost;
+import toyproject.syxxn.back_end.entity.account.Account;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PostTest {
+
+    private static final Account account = GetAccount.account;
+
+    private static final Post post = GetPost.post;
+
+    @Order(0)
+    @Test
+    void 게시글_마감여부() {
+        assertFalse(post.getIsApplicationEnd());
+    }
+
+    @Test
+    void 작성자_확인() {
+        assertEquals(account, post.getAccount());
+    }
+
+    @Test
+    void 게시글_마감() {
+        post.isEnd();
+        assertTrue(post.getIsApplicationEnd());
+    }
+
+    @Test
+    void 게시글_업데이트() {
+        post.update(
+                PostRequest.builder()
+                        .title("새해복많이받으세요")
+                        .protectionStartDate("2021-12-31")
+                        .protectionEndDate("2021-01-03")
+                        .applicationEndDate("2021-12-30")
+                        .description("배고파용")
+                        .contactInfo("012-1234-4567")
+                        .build()
+        );
+        assertEquals("새해복많이받으세요", post.getTitle());
+    }
+
+}
