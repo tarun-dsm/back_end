2a2120a4cec0f7165ea2507d1d4b0b7d7f909943
이승윤 <yyuunn17@naver.com>
2021-12-31 09:42:52 +0900

    ⚡️ :: PostRequest 내부 클래스로 나누기

--- src/main/java/toyproject/syxxn/back_end/dto/request/PostRequest.java
+++ src/main/java/toyproject/syxxn/back_end/dto/request/PostRequest.java
@@ -9,52 +9,59 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostRequest {
 
-    @NotBlank
-    @Length(min = 2, max = 30)
-    private String title;
+    private PostRequestDto post;
+    private PetRequestDto pet;
 
-    @NotBlank
-    @Length(min = 2, max = 255)
-    private String description;
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class PostRequestDto {
+        @NotBlank
+        @Length(min = 2, max = 30)
+        private String title;
 
-    @NotBlank
-    private String protectionStartDate;
+        @NotBlank
+        @Length(min = 2, max = 255)
+        private String description;
 
-    @NotBlank
-    private String protectionEndDate;
+        @NotBlank
+        private String protectionStartDate;
 
-    @NotBlank
-    private String applicationEndDate;
+        @NotBlank
+        private String protectionEndDate;
 
-    @NotBlank
-    @Length(min = 5, max = 100)
-    private String contactInfo;
+        @NotBlank
+        private String applicationEndDate;
 
-    @NotBlank
-    @Length(max = 30)
-    private String petName;
+        @NotBlank
+        @Length(min = 5, max = 100)
+        private String contactInfo;
+    }
 
-    @NotBlank
-    @Length(max = 30)
-    private String petSpecies;
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class PetRequestDto {
+        @NotBlank
+        @Length(max = 30)
+        private String petName;
 
-    @NotBlank
-    private String petSex;
+        @NotBlank
+        @Length(max = 30)
+        private String petSpecies;
 
-    @NotBlank
-    private String animalType;
+        @NotBlank
+        private String petSex;
+
+        @NotBlank
+        private String animalType;
+    }
 
     @Builder
-    public PostRequest(String title, String description, String protectionStartDate, String protectionEndDate, String applicationEndDate, String contactInfo, String petName, String petSpecies, String petSex, String animalType) {
-        this.title = title;
-        this.description = description;
-        this.protectionStartDate = protectionStartDate;
-        this.protectionEndDate = protectionEndDate;
-        this.applicationEndDate = applicationEndDate;
-        this.contactInfo = contactInfo;
-        this.petName = petName;
-        this.petSpecies = petSpecies;
-        this.petSex = petSex;
-        this.animalType = animalType;
+    public PostRequest(PostRequestDto post, PetRequestDto pet) {
+        this.post = post;
+        this.pet = pet;
     }
 }
